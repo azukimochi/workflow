2ff3fbfecdffcb21d067523c7625cd4ac50d57a4
Durian <karen.kua7@gmail.com>
2019-01-30 17:50:34 -0500

    Added react-toastify for some success animation when saving has completed

--- src/components/DragNDropCanvases/DragNDropCanvases.js
+++ src/components/DragNDropCanvases/DragNDropCanvases.js
@@ -1,41 +1,15 @@
 import React from "react"
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-// const reorder = (list, startIndex, endIndex) => {
-//     const result = Array.from(list);
-//     const [removed] = result.splice(startIndex, 1);
-//     result.splice(endIndex, 0, removed);
-//     return result;
-// };
-
-// /**
-//  * Moves an item from one list to another list.
-//  */
-// const move = (source, destination, droppableSource, droppableDestination) => {
-//     const sourceClone = Array.from(source);
-//     const destClone = Array.from(destination);
-//     const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-//     destClone.splice(droppableDestination.index, 0, removed);
-
-//     const result = {};
-//     result[droppableSource.droppableId] = sourceClone;
-//     result[droppableDestination.droppableId] = destClone;
-//     return result;
-// };
-
 const grid = 8;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: 'none',
     padding: grid * 2,
     margin: `0 0 ${grid}px 0`,
 
-    // change background colour if dragging
     background: isDragging ? 'lightgreen' : 'grey',
 
-    // styles we need to apply on draggables
     ...draggableStyle
 });
 
@@ -89,7 +63,6 @@ return(
                 {...provided.droppableProps}
                 >
                 {props.selectedActions.map((item, index) => 
-                    // return item.nextStage !== null || item.prevStage !== null ? 
                     (<Draggable
                         key={item.id}
                         draggableId={item.id}
--- src/pages/WorkflowList/WorkflowList.js
+++ src/pages/WorkflowList/WorkflowList.js
@@ -2,61 +2,30 @@ import React, { Component } from "react"
 import Workflow from "../../components/Workflow/Workflow.json"
 import CancelModal from "../../components/CancelModal/CancelModal"
 import DragNDropCanvasses from "../../components/DragNDropCanvases/DragNDropCanvases"
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const workFlowStages = Workflow.Stages
-// fake data generator
-// const getItems = (count, offset = 0) =>
-//     Array.from({ length: count }, (v, k) => k).map(k => ({
-//         id: `item-${k + offset}`,
-//         content: `item ${k + offset}`
-//     }));
 
-// a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
-};
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+}
 
-/**
- * Moves an item from one list to another list.
- */
 const move = (source, destination, droppableSource, droppableDestination) => {
-    const sourceClone = Array.from(source);
-    const destClone = Array.from(destination);
-    const [removed] = sourceClone.splice(droppableSource.index, 1);
+    const sourceClone = Array.from(source)
+    const destClone = Array.from(destination)
+    const [removed] = sourceClone.splice(droppableSource.index, 1)
 
-    destClone.splice(droppableDestination.index, 0, removed);
+    destClone.splice(droppableDestination.index, 0, removed)
 
-    const result = {};
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destClone;
-    return result;
-};
-
-// const grid = 8;
-
-// const getItemStyle = (isDragging, draggableStyle) => ({
-//     // some basic styles to make the items look a bit nicer
-//     userSelect: 'none',
-//     padding: grid * 2,
-//     margin: `0 0 ${grid}px 0`,
-
-//     // change background colour if dragging
-//     background: isDragging ? 'lightgreen' : 'grey',
-
-//     // styles we need to apply on draggables
-//     ...draggableStyle
-// });
-
-// const getListStyle = isDraggingOver => ({
-//     background: isDraggingOver ? 'lightblue' : 'lightgrey',
-//     display: 'flex',
-//     padding: grid,
-//     overflow: 'auto',
-//   });
+    const result = {}
+    result[droppableSource.droppableId] = sourceClone
+    result[droppableDestination.droppableId] = destClone
+    return result
+}
 
 class WorkflowList extends Component {
     state = {
@@ -86,18 +55,12 @@ class WorkflowList extends Component {
             })
         }
     }
-    /**
-     * A semi-generic way to handle multiple lists. Matches
-     * the IDs of the droppable container to the names of the
-     * source arrays stored in the state.
-     */
-
+  
     getList = id => this.state[this.id2List[id]];
 
     onDragEnd = result => {
         const { source, destination } = result;
 
-        // dropped outside the list
         if (!destination) {
             return;
         }
@@ -134,6 +97,7 @@ class WorkflowList extends Component {
     handleSave = event => {
         let dataStr = JSON.stringify(this.state)
         localStorage.setItem("savedWorkFlow", dataStr)
+        toast.success("Saving...")
     }
 
     handleDiscard = event => {
@@ -169,70 +133,6 @@ class WorkflowList extends Component {
             availableActions={this.state.availableActions}
             selectedActions={this.state.selectedActions}
             />
-            {/* <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppable" direction="horizontal">
-                    {(provided, snapshot) => (
-                        <div
-                            ref={provided.innerRef}
-                            style={getListStyle(snapshot.isDraggingOver)}
-                            {...provided.droppableProps}
-                            >
-                            {this.state.availableActions.map((item, index) => (
-                                <Draggable
-                                    key={item.id}
-                                    draggableId={item.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            style={getItemStyle(
-                                                snapshot.isDragging,
-                                                provided.draggableProps.style
-                                            )}>
-                                            {item.action}
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-                <br />
-                <Droppable droppableId="droppable2" direction="horizontal">
-                    {(provided, snapshot) => (
-                        <div
-                            ref={provided.innerRef}
-                            style={getListStyle(snapshot.isDraggingOver)}
-                            {...provided.droppableProps}
-                            >
-                            {this.state.selectedActions.map((item, index) => 
-                                // return item.nextStage !== null || item.prevStage !== null ? 
-                                (<Draggable
-                                    key={item.id}
-                                    draggableId={item.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div className="horizontal"
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            style={getItemStyle(
-                                                snapshot.isDragging,
-                                                provided.draggableProps.style
-                                            )}>
-                                            {item.action}
-                                        </div>
-                                    )}
-                                </Draggable>
-                                ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext> */}
 
             <div>
                 <button onClick={this.handleSave}>Save</button>
@@ -245,6 +145,18 @@ class WorkflowList extends Component {
             modalIsOpen={this.state.modalIsOpen}
             closeModal={this.closeModal}
             handleDiscard={this.handleDiscard}
+          />
+
+          <ToastContainer 
+          position="top-center"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnVisibilityChange
+          draggable
+          pauseOnHover
           />
             </div>
     </div>
